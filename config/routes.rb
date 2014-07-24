d1f14fc49a5b03472ec58ac34867a3661440bae2
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-07-24 15:57:45 -0400

    added home controller and changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'devise/registrations#new'
+  root 'home#index'
   devise_for :organizations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
