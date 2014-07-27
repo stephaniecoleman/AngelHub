44686450af1a76902647a8c52c2761593a390e7d
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-07-27 16:05:25 -0400

    added friendly message to dashboards

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if signed_in?
-      user_type = current_user.class.pluralize.underscore
+      user_type = current_user.class.name.pluralize.underscore
       # renders the current users dashboard
       # should be located in 
       #   developers/dashboard.html.erb for developers
