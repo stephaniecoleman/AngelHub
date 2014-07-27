82ea2dfd5d9f8ab4ad09fd800f1789c4de158e2c
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-07-27 15:54:59 -0400

    designed dashboard endpoint

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
   def index
-    if developer_signed_in?
-      redirect_to developers_dashboard_path
-    elsif organization_signed_in?
-      redirect_to organization_dashboard_path
+    if signed_in?
+      user_type = current_user.class.pluralize.underscore
+      # renders the current users dashboard
+      # should be located in 
+      #   developers/dashboard.html.erb for developers
+      #   organizations/dashboard.html.erb for organizations
+      render "#{user_type}/dashboard"
     else
       redirect_to action: 'new', controller: 'devise/registrations'
     end
