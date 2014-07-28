bb96e1370a82865b6e8910fc9eb4e8458a35d529
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-07-28 18:42:25 -0400

    Landing Page Created with Fake Information

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,19 @@
 module HomeHelper
+  def fake_image
+    x = Random.rand(400..600)
+    y = Random.rand(400..600)
+    "http://lorempixel.com/#{x}/#{y}/"
+  end
+
+  def fake_title
+    Faker::Company.name
+  end
+
+  def fake_content
+    Faker::Lorem.paragraph
+  end
+
+  def fake_category
+    Faker::Lorem.words(3..4)
+  end
 end
