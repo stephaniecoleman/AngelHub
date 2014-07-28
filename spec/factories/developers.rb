fc526a7ca1b926f57b321303b74d8bb6470e63b8
Shelly Seroussi <shellyser@gmail.com>
2014-07-28 12:37:07 -0400

    Developer validation

---
+++ spec/factories/developers.rb
@@ -0,0 +1,23 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  
+  factory :developer do
+    name Faker::Internet.user_name
+    email Faker::Internet.free_email
+    password Faker::Internet.password(8, 12)
+    github_url { Faker::Internet.url(name) }
+  end
+
+  factory :developer_with_projects do
+
+    ignore do
+      count 5
+    end
+
+    after(:create) do |organization, evaluator|
+      create_list(:project, evaluator.count, developer: developer)
+    end
+  end
+
+end
