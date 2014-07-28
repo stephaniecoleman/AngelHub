2a96aed8d7f8ad80f743cc8da9c245632b0917ff
Shelly Seroussi <shellyser@gmail.com>
2014-07-28 11:33:48 -0400

    Developer validation

--- spec/models/developer_spec.rb
+++ spec/models/developer_spec.rb
@@ -2,5 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Developer, :type => :model do
 
+  describe 'validations' do
+
+    context 'without a name' do
+      subject { build(:developer, :name => nil) }
+      it { should be_invalid }
+    end
+
+    context 'without a url' do
+      subject { build(:developer, :github_url => nil) }
+      it { should be_invalid }
+    end
+
+  end
   
 end
